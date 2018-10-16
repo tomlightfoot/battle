f21844a3d64452c15647140a90d93f25d230922d
tomlightfoot <tomlightfoot96@gmail.com>
2018-10-16 18:25:08 +0100

    POST/redirect/GET pattern

--- spec/features/testing_infrastracture_spec.rb
+++ spec/features/testing_infrastracture_spec.rb
@@ -1,14 +1,10 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
+feature 'Testing form fill in' do
 
   scenario 'Page fill in' do
     visit('/')
     fill_in 'player_1', with: "Sundar"
     fill_in 'player_2', with: "Tom"
     click_button 'Submit'
-    expect(page).to have_content 'Sundar vs Tom'
+    expect(page).to have_content('Sundar vs Tom')
   end
 end
