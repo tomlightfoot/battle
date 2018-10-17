6751841a102d44cf1c4c64c3ff6b6db9a4109be2
tomlightfoot <tomlightfoot96@gmail.com>
2018-10-17 12:42:26 +0100

    Adding a player class

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'player'
 
 class Battle < Sinatra::Application
 enable :sessions
@@ -8,14 +9,20 @@ enable :sessions
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    $player_1 = Player.new(params[:player_1])
+    $player_2 = Player.new(params[:player_2])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
+  end
+
+  get '/attack' do
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    erb :attack
   end
 end
--- spec/features/testing_infrastracture_spec.rb
+++ spec/features/testing_infrastracture_spec.rb
@@ -1,10 +1,21 @@
 feature 'Testing form fill in' do
-
   scenario 'Page fill in' do
-    visit('/')
-    fill_in 'player_1', with: "Sundar"
-    fill_in 'player_2', with: "Tom"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content('Sundar vs Tom')
+  end
+end
+
+feature 'View hitpoints' do
+  scenario 'View player_2 hitpoints' do
+    sign_in_and_play
+    expect(page).to have_content('Tom: 60hp')
+  end
+end
+
+feature 'Attack' do
+  scenario 'Attack player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content('Sundar attacked Tom')
   end
 end
